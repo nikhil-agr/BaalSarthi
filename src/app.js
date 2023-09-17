31a3de1b13b55bd5774c75e8e1bbb1d1c78b63e8
Mayank Shekhawat <127962246+mayanksinghshekhawat@users.noreply.github.com>
2023-09-17 10:27:29 +0530

    Update app.js with - dashboard updated with data from database

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended : true}));
 const static_path = path.join(__dirname, "../public");
 app.use(express.static(static_path));
 app.get("/",(req,res)=>{
-    res.send("index");
+  res.send("index");
 });
 
 app.get("/index2",(req,res)=>{
@@ -43,6 +43,36 @@ app.post("/index2",async (req,res)=>{
         res.status(400).send(error);
         }
 })
+
+app.get("/api/data",async(req,res)=>{
+    try {
+        const count = await Totalentries.find().countDocuments()
+        const count_poverty = await Totalentries.find({selectreason:"Poverty"}).countDocuments()
+        const count_childMarriage= await Totalentries.find({selectreason:"child marriage"}).countDocuments()
+        const count_healthIssue= await Totalentries.find({selectreason:"health_issues"}).countDocuments()
+        const count_Bullying = await Totalentries.find({selectreason:"bullying"}).countDocuments()
+        const count_FemaleChild = await Totalentries.find({selectreason:"Girl_child"}).countDocuments()
+        const count_lackInterest = await Totalentries.find({selectreason:"lack of interest"}).countDocuments()
+        const count_boys = await Totalentries.find({selectgender:"male"}).countDocuments()
+        const count_girls = await Totalentries.find({selectgender:"female"}).countDocuments()
+        const data = {
+            pieChartLabels: ['poverty', 'childMarriage', 'healthIssue','Bullying','FemaleChild','Lack OF Interest'],
+            pieChartData: [count_poverty,count_childMarriage,count_healthIssue,count_Bullying,count_FemaleChild,count_lackInterest],
+            pieChartColors: ['#EBE4D1','#26577C','#E55604','#FFCC70','#FFB000','green'],
+            total: count,
+            boys : count_boys, 
+            girls: count_girls
+          };
+        //   console.log(Totalentries.countDocuments({selectreason:"Poverty"}));
+    
+        //   res.json(pieChartData);
+          res.json(data);
+        console.log(count_FemaleChild);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+});
 
 app.listen(port,()=>{
     console.log("server is running on port 3000");
